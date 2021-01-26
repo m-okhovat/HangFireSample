739849d4d3814a8e522e4894743876b74e8cc650
مهدی اخوت <Mehdi@DESKTOP-Q9JNDQL>
2021-01-26 17:51:22 +0330

    Add controllers

---
+++ Controllers/HangFireController.cs
@@ -0,0 +1,62 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace HangFireSample.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HangFireController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("Welcome")]
+        public ActionResult Welcome(string name)
+        {
+            var jobId = BackgroundJob.Enqueue(() => SendEmail(name));
+            return Ok($"JobId {jobId} completed. Mail sent.");
+        }
+
+        [HttpGet]
+        [Route("DelayedWelcome")]
+        public ActionResult DelayedWelcome(string name) //Send email with delay
+        {
+            var jobId = BackgroundJob.Schedule(() => SendEmail(name), TimeSpan.FromMinutes(2));
+            return Ok($"JobId {jobId} completed. Mail sent.");
+        }
+
+        public void SendEmail(string name)
+        {
+            Console.WriteLine($"welcome dear {name}");
+        }
+
+        [HttpPost]
+        [Route("invoice")] //Send email recurring
+        public IActionResult Invoice(string userName)
+        {
+            RecurringJob.AddOrUpdate(() => SendInvoiceMail(userName), Cron.Monthly);
+            return Ok($"Recurring Job Scheduled. Invoice will be mailed Monthly for {userName}!");
+        }
+        public void SendInvoiceMail(string userName)
+        {
+            //Logic to Mail the user
+            Console.WriteLine($"Here is your invoice, {userName}");
+        }
+
+        [HttpPost]
+        [Route("unsubscribe")]
+        public IActionResult Unsubscribe(string userName)
+        {
+            var jobId = BackgroundJob.Enqueue(() => UnsubscribeUser(userName));
+            BackgroundJob.ContinueJobWith(jobId, () => Console.WriteLine($"Sent Confirmation Mail to {userName}"));
+            return Ok($"Unsubscribed");
+        }
+
+        public void UnsubscribeUser(string userName)
+        {
+            //Logic to Unsubscribe the user
+            Console.WriteLine($"Unsubscribed {userName}");
+        }
+
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,14 +1,9 @@
+using Hangfire;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace HangFireSample
 {
@@ -24,7 +19,8 @@ namespace HangFireSample
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddHangfire(x => x.UseSqlServerStorage(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddHangfireServer();
             services.AddControllers();
         }
 
@@ -44,6 +40,7 @@ namespace HangFireSample
             {
                 endpoints.MapControllers();
             });
+            app.UseHangfireDashboard("/mydashboard");
         }
     }
 }
